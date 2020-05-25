d5dce977c29700fd907cf8fa0e59ae5833a5efff
BradleyG248 <calvinandhobbs28@gmail.com>
2020-05-25 10:37:23 -0600

    click tracks clicks

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,9 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun click() {
-        var textView = findViewById<TextView>(R.id.textView);
-        textView.setText("This is text");
-
+    var count = 0
+    fun click(view: View) {
+        count++
+        val message = "$count clicks"
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setText(message)
     }
 }
