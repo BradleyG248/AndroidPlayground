981c8ca1140d3bf418c71b38b6a97be3d701aa92
BradleyG248 <calvinandhobbs28@gmail.com>
2020-05-25 20:07:24 -0600

    tinkered with store/state and MVC

---
+++ app/src/main/java/com/example/myapplication/controllers/AutoMinersController.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.controllers
+
+import com.example.myapplication.services.AutoMinersService
+
+class AutoMinersController {
+    private val service = AutoMinersService()
+    fun mineCheese(){
+        service.mineCheese()
+    }
+}
